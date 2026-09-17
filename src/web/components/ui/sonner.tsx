24f0fcea3b4cd413f7b3a4822f49eddc79779c37
Matt Cowger <matt@cowger.us>
2026-09-17 17:59:26 +0000

    fix(ui): show Sonner toasts at top-center

--- src/web/components/ui/sonner.tsx
+++ src/web/components/ui/sonner.tsx
@@ -42,6 +42,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme}
+      position="top-center"
       className="toaster group"
       icons={{
         success: <CircleCheckIcon className="size-4" />,
